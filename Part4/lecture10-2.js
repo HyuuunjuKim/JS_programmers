9b7efa1bbfeec585bbfa695671ae1a02e67efa36
HyuuunjuKim <gus7wn@gmail.com>
2019-06-25 21:47:40 +0900

    Part4

---
+++ Part4/lecture10-2.js
@@ -0,0 +1,3 @@
+height >=180 && gender=="male" || height >= 165 && gender == "female"
+(height >= 180 && gender == "male") || (height >= 165 && gender =="female")
+//두 식은 같은 결과를 갖지만 코드의 가독성에 대해 알 수 있음
