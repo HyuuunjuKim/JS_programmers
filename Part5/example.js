846e2b2d585ecec5bbdef597c65219ddf9416819
HyuuunjuKim <gus7wn@gmail.com>
2019-06-25 22:05:33 +0900

    Part5

---
+++ Part5/example.js
@@ -0,0 +1,17 @@
+var str="Hello";
+//길이 반환
+str.length;
+str["length"];
+//문자열 붙히기
+var str2=" World";
+str.concat(str2);
+var str3=str.concat(str2);
+"Hello".concat(" World")
+"Hello"+" World"
+
+//부분 문자열 찾기
+var tr="abcdefg";
+str.charAt(0);
+str.length;
+str.substring(2,4); // cd
+str.subsstr(2,4); //cdef
