f6d3a9f3512e706854e7b234f1ffbd8598c4755a
HyuuunjuKim <gus7wn@gmail.com>
2019-06-27 23:33:01 +0900

    배열

---
+++ Part6/ex.js
@@ -0,0 +1,11 @@
+//배열 생성하기
+var arr=[];
+var arr2=[1,2,3,4,5];
+// data type 관계없이 배열 생성 가능
+var mixed_arr=[1, 3.14,"string", true];
+//길이 return
+mixed_arr.length
+//pop은 뱌열의 마지막을 뺴는 역할 <-> push
+//shift는 배열의 첫번째 요소를 빼는 역할 <-> unshift
+//reverse는 배열을 뒤집음
+//sort는 정렬
